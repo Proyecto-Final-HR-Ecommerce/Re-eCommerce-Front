1b0497589903eade4ae864df4200d445f406d54d
EmanFun <emanuelfun.95@gmail.com>
2022-09-28 21:59:25 -0300

    feat(Form/ Product Form): create component and actions

--- cell-store/src/redux/actions.js
+++ cell-store/src/redux/actions.js
@@ -1,13 +1,8 @@
 import axios from 'axios'
 
-<<<<<<< HEAD
 export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
 export const GET_PRODUCT_BY_NAME = "GET_PRODUCT_BY_NAME"
-=======
-export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
-export const GET_DETAILS = 'GET_DETAILS';
-export const RESET = 'RESET';
->>>>>>> 0d4c7c72138421ca97486d882b744e91436a8c39
+
 
 axios.defaults.baseURL = 'http://localhost:3001'
 
@@ -26,7 +21,6 @@ export const getAllProducts = () => {
     }
 }
 
-<<<<<<< HEAD
 export function getProductByName(name){
     return async function (dispatch){
 
@@ -45,28 +39,23 @@ export function getProductByName(name){
 }
    
   
-=======
-export function getDetailId (id) {
-  return async function (dispatch) {
-   try {
-       let json = await axios.get('/countries/' + id);
-       return dispatch({
-           type: GET_DETAILS,
-           payload: json.data
-       })
-   } catch (error) {
-       console.log(error)
-   }
+export function postProduct(form, navigate){
+  return function (dispatch){
+    return axios.post('/product',form)
+      .then(res => res.data)
+      .then(payload => {
+        alert("Se a Creado un Producto Correctamente",payload)
+        //Planear redirigir a ruta con el id de reponse para la carga de category.
+        navigate('/')
+      })
+      .catch(e=> {
+        console.error(e)
+        alert('Ocurrio un Error, No fue posible crear el producto')
+        navigate('/')
+      })
+
   }
 }
-
-export function resetState () {
-  return {
-      type: RESET
-  }
-}
->>>>>>> 0d4c7c72138421ca97486d882b744e91436a8c39
-
 
 
 
