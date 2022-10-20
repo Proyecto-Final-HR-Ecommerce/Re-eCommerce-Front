7ccd966a15fbfd0f411868bee46577d06d546c26
Tomas Oyarzun <xilent312@gmail.com>
2022-10-20 09:30:21 -0300

    filter

--- cell-store/src/page/CardPagination/CardPagination.jsx
+++ cell-store/src/page/CardPagination/CardPagination.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Cards from '../../components/cards-products/Cards'
 import Paginado from '../../components/Paginado/Paginado'
-
+import Filter from '../../components/filter/Filter'
+import style from './CardPagination.module.css'
 const CardPagination = () => {
   return (
-    <div>
-          <Paginado />
-      <Cards />
-      <Paginado />
+    <div className={style.container}>
+        <div className={style.paginado}><Paginado /></div>
+          
+          <div className={style.div}>
+           <Filter/>
+          <Cards />
+          </div>
+      
+          <div className={style.paginado}><Paginado /></div>
 
     </div>
   )
