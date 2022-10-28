122a2203b85984c0c6a75b448d122a19ce868af5
Chaos <antonioscutari2@hotmail.com>
2022-10-28 07:57:44 -0300

    Movidas y modularizadas

---
+++ cell-store/src/components/Cards/Cards.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Card from "../Cards/Card";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import s from "../Cards/Cards.module.css";
+import NotFound from "../../pages/NotFound";
+
+const Cards = () => {
+  const paginados = useSelector((state) => state.product.products2);
+  const product = useSelector((state) => state.product.products);
+  const byName = useSelector((state) => state.product.byName);
+  return (
+    <div className={s.cards}>
+      {!product.hasOwnProperty("msj") ? (
+        byName === "true" ? (
+          product?.map((el) => {
+            return (
+              <div key={el._id}>
+                <Link className={s.link} key={el._id} to={`/detail/${el._id}`}>
+                  <Card
+                    key={el._id}
+                    name={el.name}
+                    image={el.image}
+                    price={el.price}
+                    id={el._id}
+                  />
+                </Link>
+              </div>
+            );
+          })
+        ) : (
+          paginados.products?.map((el) => {
+            return (
+              <div key={el._id}>
+                <Link className={s.link} key={el._id} to={`/detail/${el._id}`}>
+                  <Card
+                    key={el._id}
+                    name={el.name}
+                    image={el.image}
+                    price={el.price}
+                    id={el._id}
+                  />
+                </Link>
+              </div>
+            );
+          })
+        )
+      ) : (
+        <NotFound />
+      )}
+    </div>
+  );
+};
+
+export default Cards;
